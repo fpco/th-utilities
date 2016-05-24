79443772590b4adf5960a162201d94021c6519da
Michael Sloan <mgsloan@gmail.com>
2016-05-24 01:38:53 -0700

    Fix test-suite for GHC 8.0

--- test/TH/DeriveSpec/TH.hs
+++ test/TH/DeriveSpec/TH.hs
@@ -4,16 +4,17 @@
 
 module TH.DeriveSpec.TH where
 
-import TH.Derive
-import Language.Haskell.TH
 import Data.Proxy
+import Language.Haskell.TH
+import TH.Derive
+import TH.Utilities
 
 class InstShowBlind a
 
 instance Instantiator (InstShowBlind a) where
     runInstantiator _ preds (AppT (ConT ((== ''InstShowBlind) -> True)) ty)  []= do
         expr <- [| \_ -> "ShowBlind" |]
-        return [InstanceD preds (AppT (ConT ''Show) ty) [ValD (VarP 'show) (NormalB expr) []]]
+        return [plainInstanceD preds (AppT (ConT ''Show) ty) [ValD (VarP 'show) (NormalB expr) []]]
     runInstantiator _ _ _ _ =
         fail "Theoretically impossible case in InstShowBlind instantiator for Show"
 
@@ -23,10 +24,11 @@ instance Instantiator (InstShowConst a) where
     runInstantiator _ preds (AppT (ConT ((== ''InstShowConst) -> True)) ty) decls =
         dequalifyMethods ''InstShowConst =<<
         sequence
-        [ instanceD (return preds) [t| Show $(return ty) |] $
-            [valD (varP 'show)
-                  (normalB [| \_ -> _constResult undefined |])
-                  (map return decls)]]
+        [do headTy <- [t| Show $(return ty) |]
+            method <- valD (varP 'show)
+                (normalB [| \_ -> _constResult undefined |])
+                (map return decls)
+            return (plainInstanceD preds headTy [method])]
     runInstantiator _ _ _ _ =
         fail "Theoretically impossible case in InstShowConst instantiator for Show"
 
