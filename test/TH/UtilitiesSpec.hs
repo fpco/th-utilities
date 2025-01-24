e1dc95557e096de60a13fa526899a4553f5baa94
Daniil Iaitskov <dyaitskov@gmail.com>
2025-01-24 14:23:52 -0900

    typeRepToType-TypeLits fix TypeLits

--- test/TH/UtilitiesSpec.hs
+++ test/TH/UtilitiesSpec.hs
@@ -8,8 +8,8 @@ module TH.UtilitiesSpec where
 
 import Data.Typeable
 import Language.Haskell.TH as TH
-import Language.Haskell.TH.Lift
 import Language.Haskell.TH.Instances ()
+import Language.Haskell.TH.Lift
 import Test.Hspec as HS
 import TH.Utilities
 
@@ -27,12 +27,14 @@ spec = do
       tr (Proxy @[Int]) `shouldReturn` $(lift =<< [t| [Int] |])
     it "(Bool,Int)" $ do
       tr (Proxy @(Bool, Int)) `shouldReturn` $(lift =<< [t| (Bool, Int) |])
+    it "()" $ do
+      tr (Proxy @()) `shouldReturn` $(lift =<< [t| () |])
     it "42" $ do
-      tr (Proxy @42) `shouldReturn` $(lift =<< [t| 42 |]) --  LitT (NumTyLit 42)
+      tr (Proxy @42) `shouldReturn` $(lift =<< [t| 42 |])
     it "'c'" $ do
-      tr (Proxy @'c') `shouldReturn` $(lift =<< [t| 'c' |]) -- LitT (CharTyLit 'c')
+      tr (Proxy @'c') `shouldReturn` $(lift =<< [t| 'c' |])
     it "str" $ do
-      tr (Proxy @"hello world") `shouldReturn` $(lift =<< [t| "hello world" |]) -- LitT (StrTyLit "hello world")
+      tr (Proxy @"hello \"world") `shouldReturn` $(lift =<< [t| "hello \"world" |])
   where
     tr :: forall a. Typeable a => Proxy a -> IO Type
     tr p = runQ (typeRepToType (typeRep p))
